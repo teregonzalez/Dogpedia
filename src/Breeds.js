b03789f441bb8c7bba4464176acef49f674363f0
Tere Gonzalez <teregonzalezpro@gmail.com>
2022-01-07 16:38:10 -0300

    Modificaciones modulo Breeds

--- src/Breeds.js
+++ src/Breeds.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 //useState crea un estado interno con los datos
 //useEffect ejecuta codigo als renderizar el componente
 
-function Breeds () { //defino el componente imagenes de razas
+function Breeds () { //defino el componente de razas
     const url = 'https://dog.ceo/api/breeds/list/all'
     const [breeds, setBreeds] = useState() //declara variable de estado para las imagenes de razas
 
@@ -10,15 +10,15 @@ function Breeds () { //defino el componente imagenes de razas
         const res = await fetch(url) //trae datos a res
         const resJSON = await res.json()
         console.log(resJSON); //muestra datos en la consola
-        setBreeds(resJSON) //
+        setBreeds(resJSON) 
     }
     useEffect(() => { //para hacer peticiones de datos
         fetchApi() 
     }, [])
-    
+
     return (
         <div className="containerRazas">
-        <li>{Breeds.type}</li>
+            <div>{JSON.stringify(breeds)}</div>
         </div>
     );
 }
