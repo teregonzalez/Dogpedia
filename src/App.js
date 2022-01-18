a15efe5f2fbc30bbb018c9225aca216d77d5850c
Tere Gonzalez <teregonzalezpro@gmail.com>
2022-01-18 16:06:14 -0300

    Se elimina favoriteList, agrega filtro e imagenes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Encabezado } from "./Components/Encabezado";
 import "./index.css";
-import { Dog } from "./Components/Dog";
-import { Filtros } from "./Filtros";
 import { Breeds } from "./Components/Breeds";
 import { FavoriteList } from "./Components/FavoriteList";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,15 +10,11 @@ const App = () => {
     return (
         <div className="container-components">
             <Encabezado />
-            <Filtros />
             <div>
-            <main className="container-main">
-                <Breeds />
-            </main>
-            <section>
-                <FavoriteList />
-                <Dog />
-            </section>
+                <main className="container-main">
+                    <FavoriteList />
+                    <Breeds />
+                </main>
             </div>
         </div>
     );
